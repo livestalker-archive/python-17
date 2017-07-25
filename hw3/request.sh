b39469c7b089d2ad20254913f43583a9d4db2f9e
Alexey V. Grebenshchikov <mi.aleksio@gmail.com>
2017-07-25 11:59:49 +0300

    cleanup

--- hw3/request.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST  -H "Content-Type: application/json" -d '{"account": "horns&hoofs", "login": "admin", "method": "clients_interests", "token": "d3573aff1555cd67dccf21b95fe8c4dc8732f33fd4e32461b7fe6a71d83c947688515e36774c00fb630b039fe2223c991f045f13f24091386050205c324687a0", "arguments": {"client_ids": [1,2,3,4], "date": "20.07.2017"}}' http://127.0.0.1:8080/method/
