0224e9580308ac8814590f327cb3b8f7072a280d
Alexey V. Grebenshchikov <mi.aleksio@gmail.com>
2017-07-31 17:23:46 +0300

    change parameter name

--- hw4/httpd.py
+++ hw4/httpd.py
@@ -4,9 +4,17 @@ import argparse
 import os
 import logging
 import socket
-from multiprocessing import Process, log_to_stderr
+import StringIO
+from multiprocessing import Process as P, Queue as Q
+from multiprocessing.reduction import ForkingPickler
 
 from otus import request as req
+
+
+def forking_dumps(obj):
+    buf = StringIO.StringIO()
+    ForkingPickler(buf).dump(obj)
+    return buf.getvalue()
 
 
 class HTTPd(object):
@@ -16,11 +24,15 @@ class HTTPd(object):
         self.host = kwargs['host']
         self.port = kwargs['port']
         self.doc_root = kwargs['doc_root']
+        self.workers_count = kwargs['workers_count']
         self.listen_socket = None
+        self.clients = Q()
+        self.workers = None
 
     def run_server(self):
         """Run server"""
         self.init_listen_socket()
+        self._run_workers()
         self.listening_loop()
 
     def stop_server(self):
@@ -32,6 +44,7 @@ class HTTPd(object):
         while True:
             connection, address = self.listen_socket.accept()
             logging.debug('Client with address: %s connected.', address)
+            self.clients.put(forking_dumps(connection))
             self.process(connection)
             connection.close()
 
@@ -53,6 +66,14 @@ class HTTPd(object):
         ls.listen(1)
         self.listen_socket = ls
         logging.info('Init listenning socket.')
+
+    def worker_loop(self):
+        print os.getpid()
+        pass
+
+    def _run_workers(self):
+        workers = [P(target=self.worker_loop) for _ in range(self.workers_count)]
+        map(lambda x: x.start(), workers)
 
     @staticmethod
     def _access_log_message(request, response, bytes_sent):
@@ -77,7 +98,8 @@ if __name__ == '__main__':
 
     httpd = HTTPd(host=args.host,
                   port=args.port,
-                  doc_root=os.path.realpath(args.doc_root))
+                  doc_root=os.path.realpath(args.doc_root),
+                  workers_count=args.workers_count)
     try:
         httpd.run_server()
     except KeyboardInterrupt:
