3022f2d0910c232759f055a80444c12d157d3ac5
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2017-08-06 22:07:39 +0300

    Dockerfile based on centos/systemd

--- hw5/run_cont.sh
+++ hw5/run_cont.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 MY_DIR=`pwd`
-sudo docker run -ti --rm -p :8080:8080 hw5 /bin/bash
+sudo docker run --rm -p :8080:80 \
+                --privileged \
+                -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+                --name hw5-cont -d hw5
+sudo docker exec -i -t hw5-cont /bin/bash
