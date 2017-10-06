26a1e1dab83d1133eb5170a5bc8dbc194db7ee1d
Alexey V. Grebenshchikov <mi.aleksio@gmail.com>
2017-10-06 15:40:34 +0300

    fix to imap

--- hw9/memc_load.py
+++ hw9/memc_load.py
@@ -70,37 +70,42 @@ def parse_appsinstalled(line):
     return AppsInstalled(dev_type, dev_id, lat, lon, apps)
 
 
-def file_handler(q, res_q, device_memc, options):
+def file_handler(options):
+    fn, device_memc, options = options
     memc_pool = {}
     for d in device_memc:
         memc_pool[d] = memcache.Client([device_memc[d]])
-    try:
-        while True:
-            seq, fn = q.get(True, 10)
-            logging.info('Processing %s (seq: %s)' % (fn, seq))
-            processed = errors = 0
-            with gzip.open(fn) as fd:
-                for line in fd:
-                    line = line.strip()
-                    if not line:
-                        continue
-                    appsinstalled = parse_appsinstalled(line)
-                    if not appsinstalled:
-                        errors += 1
-                        continue
-                    memc = memc_pool.get(appsinstalled.dev_type)
-                    if not memc:
-                        errors += 1
-                        logging.error("Unknow device type: %s" % appsinstalled.dev_type)
-                        continue
-                    ok = insert_appsinstalled(memc, appsinstalled, options.dry)
-                    if ok:
-                        processed += 1
-                    else:
-                        errors += 1
-            res_q.put((seq, fn, processed, errors))
-    except Queue.Empty:
-        logging.info('%s - queue empty.' % mp.current_process().name)
+    logging.info('Processing %s.' % fn)
+    processed = errors = 0
+    with gzip.open(fn) as fd:
+        for line in fd:
+            line = line.strip()
+            if not line:
+                continue
+            appsinstalled = parse_appsinstalled(line)
+            if not appsinstalled:
+                errors += 1
+                continue
+            memc = memc_pool.get(appsinstalled.dev_type)
+            if not memc:
+                errors += 1
+                logging.error("Unknow device type: %s" % appsinstalled.dev_type)
+                continue
+            ok = insert_appsinstalled(memc, appsinstalled, options.dry)
+            if ok:
+                processed += 1
+            else:
+                errors += 1
+    if not processed:
+        fd.close()
+
+    err_rate = float(errors) / processed
+    if err_rate < NORMAL_ERR_RATE:
+        logging.info("Acceptable error rate (%s). Successfull load" % err_rate)
+    else:
+        logging.error("High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE))
+    fd.close()
+    return fn
 
 
 def res_handler(res_q):
@@ -138,30 +143,13 @@ def main(options):
         "adid": options.adid,
         "dvid": options.dvid,
     }
-    q = mp.Queue()
-    res_q = mp.Queue()
-    proc_args = (q, res_q, device_memc, options)
-    seq = 0
-    for fn in glob.iglob(options.pattern):
-        q.put((seq, fn))
-        seq += 1
-    logging.info("Worker count: %s." % mp.cpu_count())
-    proc_pool = [mp.Process(target=file_handler, args=proc_args) for _ in range(mp.cpu_count())]
-    res_proc = mp.Process(target=res_handler, args=(res_q,))
-    res_proc.daemon = True
-    res_proc.start()
-    for p in proc_pool:
-        p.daemon = True
-        p.start()
-
-    for p in proc_pool:
-        p.join()
-
-    res_q.put('STOP')
-    res_proc.join(10)
-    if res_proc.is_alive():
-        res_proc.terminate()
-
+    proc_pool = mp.Pool(int(options.workers))
+    proc_args = sorted([(fn, device_memc, options) for fn in glob.iglob(options.pattern)],
+                       key=lambda x: x[0])
+    logging.info("Worker count: %s." % options.workers)
+    for fn in proc_pool.imap(file_handler, proc_args):
+        dot_rename(fn)
+        logging.info('Rename %s (seq: %s)' % fn)
 
 def prototest():
     sample = "idfa\t1rfw452y52g2gq4g\t55.55\t42.42\t1423,43,567,3,7,23\ngaid\t7rfw452y52g2gq4g\t55.55\t42.42\t7423,424"
@@ -189,6 +177,7 @@ if __name__ == '__main__':
     op.add_option("--gaid", action="store", default="127.0.0.1:33014")
     op.add_option("--adid", action="store", default="127.0.0.1:33015")
     op.add_option("--dvid", action="store", default="127.0.0.1:33016")
+    op.add_option("-w", "--workers", action="store", default=mp.cpu_count())
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
                         format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
